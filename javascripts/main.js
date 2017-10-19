17a0a1d043faad5242a801b84df86663c480c010
daniel-certa-1228 <mfdancerta@gmail.com>
2017-10-19 13:50:46 -0500

    Added pics and formatting

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,3 +3,11 @@ console.log( "main.js" );
 
 $(".fadein").hide(0);
 $(".fadein").fadeIn(200);
+
+$('.logo-container').hide(0);
+
+$('.logo-container').each(function(logo){
+
+	$(this).delay(logo * 80).fadeIn(300);
+
+});
