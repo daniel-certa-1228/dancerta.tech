0a647ac474a2629ac881a108792b1ed37ec3134a
daniel-certa-1228 <mfdancerta@gmail.com>
2017-10-24 20:27:05 -0500

    added color and about text

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,11 @@
 "use strict";
 console.log( "main.js" );
 
+//quick fade-in of all JumboTron content during page transitions
 $(".fadein").hide(0);
 $(".fadein").fadeIn(200);
 
+//logo-animation
 $('.logo-container').hide(0);
 
 $('.logo-container').each(function(logo){
