4e53d37c0b867da6d168ba13fa6a8a24e53b5e1b
daniel-certa-1228 <mfdancerta@gmail.com>
2017-11-29 10:10:37 -0600

    removed css and dist from gitignore for deploy

---
+++ dist/built.js
@@ -0,0 +1,15 @@
+"use strict";
+console.log( "main.js" );
+
+//quick fade-in of all JumboTron content during page transitions
+$(".fadein").hide(0);
+$(".fadein").fadeIn(200);
+
+//logo-animation
+$('.logo-container').hide(0);
+
+$('.logo-container').each(function(logo){
+
+	$(this).delay(logo * 60).fadeIn(300);
+
+});
